0430e9a0036d3e96d0e97bd569181ae247aae987
Moses <moses.morare@gmail.com>
2022-12-28 16:10:40 +0200

    fixing terraform code

---
+++ src/terraform/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.37.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "TerraformState"
+    storage_account_name = "azterraformstate123"
+    container_name       = "tfstate"
+    key                  = "demo.terraform.tfstate"
+  }
+  
+}
+
+provider "azurerm" {
+  features{}
+}
+
+
+resource "azurerm_resource_group" "demo-rg" {
+  name     = "DemoRG"
+  location = "South Africa North"
+}
